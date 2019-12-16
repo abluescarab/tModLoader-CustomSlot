dc7aef13520cfb3014fecea9b4ed5baa9c6382d5
Alana Gilston <abluescarab@users.noreply.github.com>
2019-12-16 14:27:39 -0800

    Combined interior and exterior slot

--- CustomItemSlot.cs
+++ CustomItemSlot.cs
@@ -7,224 +7,89 @@ using Terraria.ID;
 using Terraria.Localization;
 using Terraria.UI;
 
-/*
- * TODO: fix number text
+/* TODO: fix number text
  * TODO: equip/unequip
  * TODO: partner slot (right-clicking vanity crashes game)
  */
 
 namespace CustomSlot {
-    public class CustomItemSlot : UIElement, ICustomSlot {
+    public class CustomItemSlot : UIElement {
         public enum ArmorType {
-            HeadArmor,
-            ChestArmor,
-            LegArmor
+            Head,
+            Chest,
+            Leg
         }
 
-        internal const int TickOffsetX = 3;
+        internal const int TickOffsetX = 6;
         internal const int TickOffsetY = 2;
 
-        private readonly SlotInterior interior;
-        private ToggleVisibilityButton toggleButton;
-        private bool forceToggleButton;
+        private Item _item;
+        private CroppedTexture2D _backgroundTexture;
+        private float _scale;
+        private ToggleVisibilityButton _toggleButton;
+        private bool _forceToggleButton;
 
         public bool ItemVisible { get; set; }
-
         public string HoverText { get; set; }
-
-        public int Context => interior.Context;
-
-        public CroppedTexture2D BackgroundTexture {
-            get => interior.BackgroundTexture;
-            set {
-                interior.BackgroundTexture = value;
-                CalculateSize(this, TickOffsetX, TickOffsetY);
-            }
-        }
-
-        public CroppedTexture2D EmptyTexture {
-            get => interior.EmptyTexture;
-            set => interior.EmptyTexture = value;
-        }
+        public int Context { get; }
+        public Func<Item, bool> IsValidItem { get; set; }
+        public CroppedTexture2D EmptyTexture { get; set; }
 
         public Item Item {
-            get => interior.Item;
-            set => interior.Item = value;
-        }
-
-        public Func<Item, bool> IsValidItem {
-            get => interior.IsValidItem;
-            set => interior.IsValidItem = value;
+            get => _item;
+            set => _item = value;
         }
 
         public float Scale {
-            get => interior.Scale;
+            get => _scale;
             set {
-                interior.Scale = value;
-                CalculateSize(this, TickOffsetX, TickOffsetY);
+                _scale = value;
+                CalculateSize();
+            }
+        }
+
+        public CroppedTexture2D BackgroundTexture {
+            get => _backgroundTexture;
+            set {
+                _backgroundTexture = value;
+                CalculateSize();
             }
         }
 
         public bool ForceToggleButton {
-            get => forceToggleButton;
+            get => _forceToggleButton;
             set {
-                forceToggleButton = value;
-                bool hasButton = ForceToggleButton || HasToggleButton(Context);
+                _forceToggleButton = value;
+                bool hasButton = _forceToggleButton || HasToggleButton(Context);
 
                 if(!hasButton) {
-                    if(toggleButton == null) return;
+                    if(_toggleButton == null) return;
 
-                    RemoveChild(toggleButton);
-                    toggleButton = null;
+                    RemoveChild(_toggleButton);
+                    _toggleButton = null;
                 }
                 else {
-                    toggleButton = new ToggleVisibilityButton();
-                    Append(toggleButton);
+                    _toggleButton = new ToggleVisibilityButton();
+                    Append(_toggleButton);
                 }
             }
         }
 
         public CustomItemSlot(int context = ItemSlot.Context.InventoryItem, float scale = 1f,
-            ArmorType defaultArmorIcon = ArmorType.HeadArmor) {
-            Texture2D backgroundTexture = GetBackgroundTexture(context);
+            ArmorType defaultArmorIcon = ArmorType.Head) {
+            Texture2D backgroundTex = GetBackgroundTexture(context);
 
-            interior = new SlotInterior(
-                context,
-                scale,
-                null,
-                new CroppedTexture2D(backgroundTexture),
-                GetEmptyTexture(context, defaultArmorIcon));
-
-            Append(interior);
-
+            Context = context;
+            _scale = scale;
+            _backgroundTexture = new CroppedTexture2D(backgroundTex);
+            EmptyTexture = GetEmptyTexture(context, defaultArmorIcon);
             ItemVisible = true;
             ForceToggleButton = false;
 
-            CalculateSize(this, TickOffsetX, TickOffsetY);
-        }
+            Item = new Item();
+            Item.SetDefaults();
 
-        internal static void CalculateSize(ICustomSlot slot, int offsetX, int offsetY) {
-            if(slot.BackgroundTexture == CroppedTexture2D.Empty) return;
-
-            float width = (slot.BackgroundTexture.Texture.Width * slot.Scale) + offsetX;
-            float height = (slot.BackgroundTexture.Texture.Height * slot.Scale) + offsetY;
-
-            UIElement element = (UIElement)slot;
-            element.Width.Set(width, 0f);
-            element.Height.Set(height, 0f);
-        }
-
-        internal class SlotInterior : UIElement, ICustomSlot {
-            private Item item;
-            private CroppedTexture2D backgroundTexture;
-            private float scale;
-
-            public int Context { get; }
-            public Func<Item, bool> IsValidItem { get; set; }
-            public CroppedTexture2D EmptyTexture { get; set; }
-
-            public Item Item {
-                get => item;
-                set => item = value;
-            }
-
-            public CroppedTexture2D BackgroundTexture {
-                get => backgroundTexture;
-                set {
-                    backgroundTexture = value;
-                    CalculateSize(this, 0, 0);
-                }
-            }
-
-            public float Scale {
-                get => scale;
-                set {
-                    scale = value;
-                    CalculateSize(this, 0, 0);
-                }
-            }
-
-            internal SlotInterior(int context, float scale, Func<Item, bool> isValidItem,
-                CroppedTexture2D backgroundTexture, CroppedTexture2D emptyTexture) {
-                Context = context;
-                Scale = scale;
-                IsValidItem = isValidItem;
-                BackgroundTexture = backgroundTexture;
-                EmptyTexture = emptyTexture;
-                Item = new Item();
-                Item.SetDefaults();
-            }
-
-            protected override void DrawSelf(SpriteBatch spriteBatch) {
-                DoDraw(spriteBatch);
-
-                if(ContainsPoint(Main.MouseScreen) && !PlayerInput.IgnoreMouseInterface) {
-                    Main.LocalPlayer.mouseInterface = true;
-
-                    CustomItemSlot parent = (CustomItemSlot)Parent;
-
-                    if(parent.toggleButton != null && parent.toggleButton.ContainsPoint(Main.MouseScreen)) return;
-
-                    if(IsValidItem == null || IsValidItem(Main.mouseItem) || Main.mouseItem.type == 0) {
-                        ItemSlot.Handle(ref item, Context);
-
-                        if(!string.IsNullOrEmpty(parent.HoverText)) {
-                            Main.hoverItemName = parent.HoverText;
-                        }
-                    }
-                }
-            }
-
-            private void DoDraw(SpriteBatch spriteBatch) {
-                Rectangle parentRectangle = Parent.GetDimensions().ToRectangle();
-                Rectangle rectangle = GetDimensions().ToRectangle();
-                Texture2D itemTexture = EmptyTexture.Texture;
-                Rectangle itemRectangle = EmptyTexture.Rectangle;
-                Color color = Color.White * 0.35f;
-
-                if(Item.stack > 0) {
-                    itemTexture = Main.itemTexture[Item.type];
-                    itemRectangle = Main.itemAnimations[Item.type] != null
-                        ? Main.itemAnimations[Item.type].GetFrame(itemTexture) : itemTexture.Frame();
-                    color = Color.White;
-                }
-
-                spriteBatch.Draw(
-                    BackgroundTexture.Texture,
-                    parentRectangle.TopLeft() + new Vector2(0, TickOffsetY),
-                    BackgroundTexture.Rectangle,
-                    Color.White * 0.8f,
-                    0f,
-                    Vector2.Zero,
-                    Scale,
-                    SpriteEffects.None,
-                    1f);
-
-                spriteBatch.Draw(
-                    itemTexture,
-                    rectangle.Center(),
-                    itemRectangle,
-                    color,
-                    0f,
-                    new Vector2(itemRectangle.Center.X - itemRectangle.Location.X,
-                                itemRectangle.Center.Y - itemRectangle.Location.Y - TickOffsetY),
-                    Scale,
-                    SpriteEffects.None,
-                    0f);
-
-                // TODO: fix number text
-                //ChatManager.DrawColorCodedStringWithShadow(
-                //    spriteBatch,
-                //    Main.fontItemStack,
-                //    Item.stack.ToString(),
-                //    position + new Vector2(10f, 26f) * Scale,
-                //    Color.White,
-                //    0f,
-                //    Vector2.Zero,
-                //    new Vector2(Scale),
-                //    -1f,
-                //    Scale);
-            }
+            CalculateSize();
         }
 
         internal class ToggleVisibilityButton : UIElement {
@@ -255,12 +120,81 @@ namespace CustomSlot {
                     slot.ItemVisible ? Main.inventoryTickOnTexture : Main.inventoryTickOffTexture;
 
                 Left.Set(parentRectangle.Width - Width.Pixels + TickOffsetX, 0f);
+                Top.Set(-TickOffsetY, 0f);
 
                 spriteBatch.Draw(
                     tickTexture,
-                    new Vector2(parentRectangle.Right - tickTexture.Width + TickOffsetX, parentRectangle.Top),
+                    GetDimensions().Position(),
                     Color.White * 0.7f);
             }
+        }
+
+        protected override void DrawSelf(SpriteBatch spriteBatch) {
+            DoDraw(spriteBatch);
+
+            if(ContainsPoint(Main.MouseScreen) && !PlayerInput.IgnoreMouseInterface) {
+                Main.LocalPlayer.mouseInterface = true;
+
+                if(_toggleButton != null && _toggleButton.ContainsPoint(Main.MouseScreen)) return;
+
+                if(IsValidItem == null || Main.mouseItem.stack == 0 || IsValidItem(Main.mouseItem)) {
+                    ItemSlot.Handle(ref _item, Context);
+
+                    if(!string.IsNullOrEmpty(HoverText)) {
+                        Main.hoverItemName = HoverText;
+                    }
+                }
+            }
+        }
+
+        private void DoDraw(SpriteBatch spriteBatch) {
+            Rectangle rectangle = GetDimensions().ToRectangle();
+            Texture2D itemTexture = EmptyTexture.Texture;
+            Rectangle itemRectangle = EmptyTexture.Rectangle;
+            Color color = Color.White * 0.35f;
+
+            if(Item.stack > 0) {
+                itemTexture = Main.itemTexture[Item.type];
+                itemRectangle = Main.itemAnimations[Item.type] != null ?
+                    Main.itemAnimations[Item.type].GetFrame(itemTexture) : itemTexture.Frame();
+                color = Color.White;
+            }
+
+            spriteBatch.Draw(
+                BackgroundTexture.Texture,
+                rectangle.TopLeft(),
+                BackgroundTexture.Rectangle,
+                Color.White * 0.8f,
+                0f,
+                Vector2.Zero,
+                Scale,
+                SpriteEffects.None,
+                1f);
+
+            spriteBatch.Draw(
+                itemTexture,
+                rectangle.Center(),
+                itemRectangle,
+                color,
+                0f,
+                new Vector2(itemRectangle.Center.X - itemRectangle.Location.X,
+                            itemRectangle.Center.Y - itemRectangle.Location.Y),
+                Scale,
+                SpriteEffects.None,
+                0f);
+        }
+
+        /// <summary>
+        /// Calculate the size of the slot based on background texture and scale.
+        /// </summary>
+        internal void CalculateSize() {
+            if(BackgroundTexture == CroppedTexture2D.Empty) return;
+
+            float width = BackgroundTexture.Texture.Width * Scale;
+            float height = BackgroundTexture.Texture.Height * Scale;
+
+            Width.Set(width, 0f);
+            Height.Set(height, 0f);
         }
 
         /// <summary>
@@ -306,32 +240,32 @@ namespace CustomSlot {
         /// <param name="context">slot context</param>
         /// <param name="armorType">type of equipment in the slot</param>
         /// <returns>empty texture of the slot</returns>
-        public static CroppedTexture2D GetEmptyTexture(int context, ArmorType armorType = ArmorType.HeadArmor) {
+        public static CroppedTexture2D GetEmptyTexture(int context, ArmorType armorType = ArmorType.Head) {
             int frame = -1;
 
             switch(context) {
                 case ItemSlot.Context.EquipArmor:
                     switch(armorType) {
-                        case ArmorType.HeadArmor:
+                        case ArmorType.Head:
                             frame = 0;
                             break;
-                        case ArmorType.ChestArmor:
+                        case ArmorType.Chest:
                             frame = 6;
                             break;
-                        case ArmorType.LegArmor:
+                        case ArmorType.Leg:
                             frame = 12;
                             break;
                     }
                     break;
                 case ItemSlot.Context.EquipArmorVanity:
                     switch(armorType) {
-                        case ArmorType.HeadArmor:
+                        case ArmorType.Head:
                             frame = 3;
                             break;
-                        case ArmorType.ChestArmor:
+                        case ArmorType.Chest:
                             frame = 9;
                             break;
-                        case ArmorType.LegArmor:
+                        case ArmorType.Leg:
                             frame = 15;
                             break;
                     }
